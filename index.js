63fa974ad1b0180f2e369e34e248ceaa2cec1692
fouad8080 <fouad1563ghgg@gmail.com>
2026-04-25 15:06:33 +0100

    add html file and some style

---
+++ index.js
@@ -0,0 +1,9 @@
+const cityinput=document.getElementById("cityInput");
+const cityName=document.getElementById("cityName");
+const temperature=document.getElementById("temperature");
+const description=document.getElementById("description");
+const humidity=document.getElementById("humidity");
+const weatherEmoji=document.getElementById("weatherEmoji");
+const windSpeed=document.getElementById("windSpeed");
+const errorMessage=document.getElementById("errorMessage");
+
